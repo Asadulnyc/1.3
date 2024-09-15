431a36dfc8d5f06714350da09e5cb3a9c3469b39
Asadulnyc <asadulk@nycstudents.net>
2024-09-15 23:21:14 +0000

    commit

--- Main.java
+++ Main.java
@@ -9,11 +9,10 @@ public class Main
         int first = s.nextInt();
         System.out.println("What operation would you like to do?");
         String operation = s.next();
-        while((operation != "+")|(operation != "-")|(operation != "/")|(operation != "*")|(operation != "%")){
+        while(!(operation.equals("+") || operation.equals("-") || operation.equals("/") || operation.equals("*") || operation.equals("%"))){
             System.out.println("Invalid operation.");
             System.out.println("What operation would you like to do?");
             operation = s.next();
-            System.out.println(operation);
         }
         System.out.println("Enter your second number.");
         int second = s.nextInt();
@@ -27,7 +26,12 @@ public class Main
         }
         if(operation.equals("/"))
         {
-            System.out.println("Answer: " + (first / second));
+            if(second == 0){
+                System.out.println("Impossible Operation due to 0 denominator.");
+            }
+            else{
+                System.out.println("Answer: " + (first / second));
+            }
         }
         if(operation.equals("-"))
         {
@@ -35,7 +39,12 @@ public class Main
         }
         if(operation.equals("%"))
         {
-            System.out.println("Answer: " + (first % second));
+            if(second == 0){
+                System.out.println("Impossible Operation due to 0 denominator.");
+            }
+            else{
+                System.out.println("Answer: " + (first % second));
+            }
         }
         s.close();
     }
